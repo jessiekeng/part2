3dace0a743412e67c1e56cb6d1214d1793d638dd
jessiekeng <zixingkeng@gmail.com>
2025-12-27 19:28:30 +0800

    Initialize part2 with courseinfo

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm.jsx'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const exists = persons.some(
+      person => person.name.toLowerCase() === newName.toLowerCase()
+    )
+
+    if (exists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter value={filter} onChange={e => setFilter(e.target.value)} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        handleNameChange={e => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={e => setNewNumber(e.target.value)}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
