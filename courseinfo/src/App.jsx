55b2dc8d169bcc56029e8990d2dc738a8842c046
jessiekeng <zixingkeng@gmail.com>
2025-12-31 10:43:12 +0800

    test commit from vscode

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import personService from './services/person'
+import personService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -11,59 +11,67 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    personService.getAll().then(data => setPersons(data))
+    personService.getAll().then(initialPersons => {
+      setPersons(initialPersons)
+    })
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-    const existingPerson = persons.find(p => p.name === newName)
+    const existingPerson = persons.find(p => p.name.toLowerCase() === newName.toLowerCase())
 
     if (existingPerson) {
-      if (window.confirm(`${newName} is already in phonebook, replace the number?`)) {
-        const updatedPerson = { ...existingPerson, number: newNumber }
-        personService.update(existingPerson.id, updatedPerson)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const changedPerson = { ...existingPerson, number: newNumber }
+        personService.update(existingPerson.id, changedPerson)
           .then(returnedPerson => {
             setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(() => {
+            alert(`Information of ${newName} has already been removed from server`)
+            setPersons(persons.filter(p => p.id !== existingPerson.id))
           })
       }
       return
     }
 
     const personObject = { name: newName, number: newNumber }
-    personService.create(personObject)
-      .then(returnedPerson => setPersons(persons.concat(returnedPerson)))
-
-    setNewName('')
-    setNewNumber('')
+    personService.create(personObject).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewNumber('')
+    })
   }
 
   const handleDelete = (id, name) => {
     if (window.confirm(`Delete ${name}?`)) {
-      personService.remove(id)
-        .then(() => setPersons(persons.filter(p => p.id !== id)))
+      personService.remove(id).then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+      })
     }
   }
 
-  const personsToShow = persons.filter(person =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  )
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filter} onChange={e => setFilter(e.target.value)} />
-
-      <h3>Add a new</h3>
-      <PersonForm
+      <Filter value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <h3>add a new</h3>
+      <PersonForm 
         onSubmit={addPerson}
         newName={newName}
-        handleNameChange={e => setNewName(e.target.value)}
+        handleNameChange={(e) => setNewName(e.target.value)}
         newNumber={newNumber}
-        handleNumberChange={e => setNewNumber(e.target.value)}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
       />
-
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} handleDelete={handleDelete} />
+
+      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
     </div>
   )
 }
--- courseinfo/src/components/PersonForm.jsx
+++ courseinfo/src/components/PersonForm.jsx
@@ -1,14 +1,8 @@
 const PersonForm = ({ onSubmit, newName, handleNameChange, newNumber, handleNumberChange }) => (
   <form onSubmit={onSubmit}>
-    <div>
-      name: <input value={newName} onChange={handleNameChange} />
-    </div>
-    <div>
-      number: <input value={newNumber} onChange={handleNumberChange} />
-    </div>
-    <div>
-      <button type="submit">add</button>
-    </div>
+    <div>name: <input value={newName} onChange={handleNameChange} /></div>
+    <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+    <div><button type="submit">add</button></div>
   </form>
 )
 
