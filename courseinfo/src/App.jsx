3914adfa05daa11f9e1fe84de6687e854befcc13
jessiekeng <zixingkeng@gmail.com>
2026-01-01 14:09:47 +0800

    exercise 2.16 2.17 - Refactor the course information app

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,77 +1,32 @@
-import { useState, useEffect } from 'react'
-import personService from './services/persons'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+import Course from './components/Course'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-
-  useEffect(() => {
-    personService.getAll().then(initialPersons => {
-      setPersons(initialPersons)
-    })
-  }, [])
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const existingPerson = persons.find(p => p.name.toLowerCase() === newName.toLowerCase())
-
-    if (existingPerson) {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        const changedPerson = { ...existingPerson, number: newNumber }
-        personService.update(existingPerson.id, changedPerson)
-          .then(returnedPerson => {
-            setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
-            setNewName('')
-            setNewNumber('')
-          })
-          .catch(() => {
-            alert(`Information of ${newName} has already been removed from server`)
-            setPersons(persons.filter(p => p.id !== existingPerson.id))
-          })
-      }
-      return
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        { name: 'Fundamentals of React', exercises: 10, id: 1 },
+        { name: 'Using props to pass data', exercises: 7, id: 2 },
+        { name: 'State of a component', exercises: 14, id: 3 },
+        { name: 'Redux', exercises: 11, id: 4 }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        { name: 'Routing', exercises: 3, id: 1 },
+        { name: 'Middlewares', exercises: 7, id: 2 }
+      ]
     }
-
-    const personObject = { name: newName, number: newNumber }
-    personService.create(personObject).then(returnedPerson => {
-      setPersons(persons.concat(returnedPerson))
-      setNewName('')
-      setNewNumber('')
-    })
-  }
-
-  const handleDelete = (id, name) => {
-    if (window.confirm(`Delete ${name}?`)) {
-      personService.remove(id).then(() => {
-        setPersons(persons.filter(p => p.id !== id))
-      })
-    }
-  }
-
-  const personsToShow = filter === ''
-    ? persons
-    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+  ]
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter value={filter} onChange={(e) => setFilter(e.target.value)} />
-      <h3>add a new</h3>
-      <PersonForm 
-        onSubmit={addPerson}
-        newName={newName}
-        handleNameChange={(e) => setNewName(e.target.value)}
-        newNumber={newNumber}
-        handleNumberChange={(e) => setNewNumber(e.target.value)}
-      />
-      <h3>Numbers</h3>
-
-      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   )
 }
